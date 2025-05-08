9583a65c6bfbdfc5a49a93fd43706e773218fa0e
Michelle Glebova <147517074+michlleee@users.noreply.github.com>
2025-05-08 16:32:07 +0700

    moved files and extracted method in BankingApp.java

---
+++ bankmanagement/src/problems/User.java
@@ -0,0 +1,90 @@
+package problems;
+import java.sql.*;
+import java.util.*;
+
+/*
+ * smell code: long method
+ * reason: function seperti register() dan login() memiliki kode yang numpuk dengan tanggung jawab beda
+ *         seperti handle user input, sql queries, dan validasi input.
+ * solution: extract method
+ */
+
+public class User {
+    private Connection connection;
+    private Scanner scanner;
+
+    public User(Connection connection, Scanner scanner){
+        this.connection = connection;
+        this.scanner = scanner;
+    }
+
+    public void register(){
+        scanner.nextLine();
+        System.out.print("Full Name: ");
+        String full_name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        if(user_exist(email)) {
+            System.out.println("User Already Exists for this Email Address!!");
+            return;
+        }
+        String register_query = "INSERT INTO users(full_name, email, password) VALUES(?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(register_query);
+            preparedStatement.setString(1, full_name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, password);
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Registration Successfull!");
+            } else {
+                System.out.println("Registration Failed!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String login(){
+        scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        String login_query = "SELECT * FROM users WHERE email = ? AND password = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(login_query);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return email;
+            }else{
+                return null;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean user_exist(String email){
+        String query = "SELECT * FROM users WHERE email = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
